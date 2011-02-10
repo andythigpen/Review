fd964c8b0b0f7e9bb7b3fb63d73759a7f4e3a2c5
andyt <andyt05@gmail.com>
2011-02-09 21:06:25 -0600

    Committing prototype work. This includes initial testing done with the devise gem.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Review::Application.config.secret_token = '0f0e44a581ef53f19bd39c4dcd03bf11a444a69a467e4de3a6ed152f71ea68906ecc6c46705869fb34176231fb70bfde0dcc1978a6cf375610b919bfb6a92488'
---
+++ public/javascripts/application.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function() {
+  $(".button").hover(
+  	function() { $(this).addClass('ui-state-hover'); }, 
+  	function() { $(this).removeClass('ui-state-hover'); }
+  );
+});

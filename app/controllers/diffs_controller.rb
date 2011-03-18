4e9bc3157f3f8767b73c97f1b66c2b2386312256
andyt <andyt05@gmail.com>
2011-03-18 17:45:39 -0500

    Fixes filename parsing issue.  Fixes labels for new review events.

--- app/controllers/diffs_controller.rb
+++ app/controllers/diffs_controller.rb
@@ -15,7 +15,7 @@ class DiffsController < ApplicationController
 
     diff_started = false
     contents.each do |line|
-      if line =~ /^---\s+([\w+\/\\:]+)/
+      if line =~ /^---\s+([\w\/\\:\.-]+)/
         if diff_started
           @diff = Diff.new diff_params
           break if not @diff.save
@@ -23,7 +23,7 @@ class DiffsController < ApplicationController
         end
         diff_params[:origin_file] = $1
         diff_started = true
-      elsif line =~ /^\+\+\+\s+([\w+\/\\:]+)/
+      elsif line =~ /^\+\+\+\s+([\w\/\\:\.-]+)/
         diff_params[:updated_file] = $1
       elsif line =~ /^(\+|-| |@)/
         diff_params[:contents] += line

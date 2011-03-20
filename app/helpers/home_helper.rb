d3805de1e5f32fe88cab74f330c334f0133ab1b8
andyt <andyt05@gmail.com>
2011-03-20 10:51:23 -0500

    Adds submitted changesets to Recent Activity stream on user's home page.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,4 +2,15 @@ module HomeHelper
   def recent_comments(days, limit)
     Comment.where(:updated_at => days..Time.now).order("updated_at DESC").limit(limit)
   end
+
+  def recent_submitted_changesets(days, limit)
+    changesets = Changeset.where(:updated_at => days..Time.now, :submitted => true).order("updated_at DESC").limit(limit)
+  end
+
+  def all_recent(days, limit)
+    comments = recent_comments(days, limit)
+    changesets = recent_submitted_changesets(days, limit)
+    combined = comments | changesets
+    combined.sort! {|a,b| b.updated_at <=> a.updated_at}
+  end
 end

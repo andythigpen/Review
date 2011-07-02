ce234685d7b9b5772cae3b2fd5ee09c13d45df12
andyt <andyt05@gmail.com>
2011-07-02 15:51:59 -0500

    Adds ability to search via profile names.

--- app/controllers/reviewers_controller.rb
+++ app/controllers/reviewers_controller.rb
@@ -4,7 +4,21 @@ class ReviewersController < ApplicationController
   def show
     @users = User.where("username LIKE ? OR email LIKE ?", 
                         "#{params[:term]}%", "#{params[:term]}%")
-    @usernames = @users.map { |u| { :label => u.username, :id => u.id } }
+    @profiles = Profile.where("first_name LIKE ? or last_name LIKE ?",
+                              "%#{params[:term]}%", "#{params[:term]}%")
+    @profile_usernames = @profiles.map do |p| 
+      label = "#{p.user.username} (#{p.first_name} #{p.last_name})"
+      { :label => label, :id => p.user.id }
+    end
+    @usernames = @users.map do |u| 
+      label = "#{u.username}"
+      if not u.profile.nil?
+        label += " (#{u.profile.first_name} #{u.profile.last_name})"
+      end
+      { :label => label, :id => u.id } 
+    end
+    # labels/id should match so duplicates are removed!
+    @usernames = @usernames | @profile_usernames
 
     respond_to do |format|
       format.json { render :json => @usernames }

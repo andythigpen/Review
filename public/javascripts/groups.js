817c806ee402ea91637b0e373e4d7ec0a9dadb45
andyt <andyt05@gmail.com>
2012-02-17 23:29:40 -0600

    Fix groups layout.

--- public/javascripts/groups.js
+++ public/javascripts/groups.js
@@ -1,28 +1,45 @@
 function remove_group(group_id) {
-  $("#remove-group-dialog").dialog({
-    modal: true,
-    title: "Remove Group",
-    buttons: {
-      "Remove": function() {
-        show_ajax_loader(this);
-        $.ajax({
-          url: "/groups/"+group_id,
-          type: "DELETE",
-          dataType: "json",
-          success: function(msg) {
-            $('#remove-group-dialog').dialog('close');
-            if (msg.status == "ok") {
-              window.location.href = "/groups";
-            }
-          }
-        });
-      },
-      "Cancel": function() {
-        $(this).dialog('close');
+  $("#remove-group-dialog").data("group-id", group_id).modal('show');
+  // $("#remove-group-dialog").dialog({
+  //   modal: true,
+  //   title: "Remove Group",
+  //   buttons: {
+  //     "Remove": function() {
+  //       show_ajax_loader(this);
+  //       $.ajax({
+  //         url: "/groups/"+group_id,
+  //         type: "DELETE",
+  //         dataType: "json",
+  //         success: function(msg) {
+  //           $('#remove-group-dialog').dialog('close');
+  //           if (msg.status == "ok") {
+  //             window.location.href = "/groups";
+  //           }
+  //         }
+  //       });
+  //     },
+  //     "Cancel": function() {
+  //       $(this).dialog('close');
+  //     }
+  //   },
+  //   open: function() {
+  //     hide_ajax_loader(this);
+  //   }
+  // });
+}
+
+function remove_group_modal() {
+  var group_id = $("#remove-group-dialog").data("group-id");
+  //       show_ajax_loader(this);
+  $.ajax({
+    url: "/groups/"+group_id,
+    type: "DELETE",
+    dataType: "json",
+    success: function(msg) {
+      $('#remove-group-dialog').modal('hide');
+      if (msg.status == "ok") {
+        window.location.href = "/groups";
       }
-    },
-    open: function() {
-      hide_ajax_loader(this);
     }
   });
 }

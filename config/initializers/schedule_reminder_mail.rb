5e0091b4f92ded7f98a97920d0816f79ec64f337
andyt <andyt05@gmail.com>
2012-03-10 23:18:45 -0600

    Fix issue that broke db:schema:load due to initializer.

--- config/initializers/schedule_reminder_mail.rb
+++ config/initializers/schedule_reminder_mail.rb
@@ -1,2 +1,4 @@
 
-ReminderMailJob.schedule(Time.now.end_of_day + 5.hour)
+Rails::Application.initializer "reminder_mail_job.schedule", :after => "active_record.initialize_database" do |app|
+  ReminderMailJob.schedule(Time.now.end_of_day + 5.hour)
+end

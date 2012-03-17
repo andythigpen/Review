50c4e6b19bd2a50256fcc6b063647cd585136230
andyt <andyt05@gmail.com>
2012-03-16 21:07:22 -0500

    Fix #80, Warning initialized constant in production.

--- app/models/email_settings.rb
+++ app/models/email_settings.rb
@@ -1,8 +1,4 @@
 class EmailSetting
-  NONE = -1
-  INSTANT = 0
-  DAILY = 1
-  WEEKLY = 7
   attr_accessor :participant, :owner
 
   def initialize
---
+++ config/initializers/constants.rb
@@ -0,0 +1,7 @@
+class EmailSetting
+  NONE = -1
+  INSTANT = 0
+  DAILY = 1
+  WEEKLY = 7
+end
+
